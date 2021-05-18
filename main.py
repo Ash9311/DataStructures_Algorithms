95c1053052c4bbfe1d375a16f344d9b5262f7045
Ashutosh Mulky <>
2021-05-18 15:40:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def maxSumSubArray(a):
+    maxx = 0
+    curr = 0
+
+    for i in range(len(a)):
+        curr += a[i]
+        if(curr>maxx):
+            maxx = curr
+        
+        if(curr<0):
+            curr=0
+    return maxx
+
+a= [1,-2,2,-3,44,-1,41]
+print(maxSumSubArray(a))
+
+
