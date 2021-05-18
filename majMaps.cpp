50c153af73b04e8f197902e343e059a4187c678e
Ashutosh Mulky <>
2021-05-18 17:22:29 +0000

    added majMaps

---
+++ majMaps.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> counter;
+        
+        for(int num: nums){
+            counter[num] = counter[num] + 1;
+            if(counter[num]>nums.size()/2){
+                return num;
+            }
+        }
+        return 0;
+    }
+
+};
