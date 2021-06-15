403fb7f450026b707c2925b013d4154bb567bccc
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2021-06-15 23:50:02 +0530

    Create BT_maxDepth.java

---
+++ Trees/BT_maxDepth.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        int left_depth = maxDepth(root.left)+1;
+        int right_depth = maxDepth(root.right)+1;
+        return left_depth>right_depth?left_depth:right_depth;
+    }
+}
