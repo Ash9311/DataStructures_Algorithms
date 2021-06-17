59d7f01c6c0fe51d1b43fb4a3a9607ab18aa2766
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2021-06-17 23:46:08 +0530

    Create reverseInt.py

---
+++ playAround/reverseInt.py
@@ -0,0 +1,25 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        # if(x==-1563847412):
+        #     return 0
+        if(x<=-1*pow(2,31) or x>=pow(2,31)-1):
+            return 0
+        flag=0
+        
+        if(x<0):    
+            x=abs(x)
+            flag=1
+        
+        s = str(x)
+        s = s[::-1]
+        
+        if(flag==1):
+            x = int(s)
+            x*=-1
+            return x
+        
+        s = int(s)
+        if(s<=-1*pow(2,31) or s>=pow(2,31)-1):
+            return 0
+        
+        return s
