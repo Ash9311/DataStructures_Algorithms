36eaee15bff1ae94cf1be489e2fdcc6da20b5d0d
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2021-06-19 22:03:20 +0530

    Create ShuffleArray.java

---
+++ Arrays/ShuffleArray.java
@@ -0,0 +1,10 @@
+// shuffle array according to index
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char[] c = new char[indices.length];
+        for(int i=0;i<indices.length;i++){
+            c[indices[i]]=s.charAt(i);
+        }
+        return new String(c);
+    }
+}
