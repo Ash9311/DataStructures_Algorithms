aaa34200b658f09301760b2ddd0c782c782fbe83
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2021-06-24 23:01:50 +0530

    Update experiments.cpp

--- playAround/experiments.cpp
+++ playAround/experiments.cpp
@@ -32,3 +32,30 @@ class Solution:
         return Integer.parseInt(("" + num).replaceFirst("1", "2"));
     }
 --------------------------------------------------------------------
+    
+    import java.util.*;
+
+public class Main{
+
+     void permutation(String str){
+            permutation(str,"");
+        }
+
+        void permutation(String str, String prefix){
+            if(str.length()==0){
+                System.out.println(prefix);
+            }
+            else{
+            }
+                for(int i=0;i<str.length();i++){
+                    String rem = str.substring(0,i) + str.substring(i+1);
+                    permutation(rem,prefix + str.charAt(i));
+            }
+        }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Main m = new Main();
+        m.permutation("ashu");
+    }
+}
