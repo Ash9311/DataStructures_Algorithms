3dfdfaca2b00724f5b38ad26c4a8727389115d1a
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2021-06-27 00:24:49 +0530

    Update experiments.cpp

--- playAround/experiments.cpp
+++ playAround/experiments.cpp
@@ -118,3 +118,10 @@ public class Main{
 
 nt best = Arrays.stream(b).min().getAsInt();
         int worst = Arrays.stream(b).max().getAsInt();
+
+-------------------------------------------------------------------
+	class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        return (String.join("",word1).equals(String.join("",word2)));
+    }
+}
