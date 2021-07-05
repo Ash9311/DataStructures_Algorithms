0f953c8835e4588964efb82bd39241ede6fd31d9
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2021-07-05 09:46:02 +0000

    added functionalities in Queue impole

--- Queue/QueueImplementation.java
+++ Queue/QueueImplementation.java
@@ -23,7 +23,7 @@ public class Queue{
     }
 
     void enQueue(int element){
-        if(isFull){
+        if(isFull()){
             System.out.println("Queue is Full");
         }
         else{
@@ -31,10 +31,70 @@ public class Queue{
                 front=0;
             }
             rear++;
-            iteam[rear]=element;
+            items[rear]=element;
             System.out.println("Inserted element "+element);
         }
     }
-
     
+    int deQueue(){
+        int element;
+        if(isEmpty()){
+            System.out.println("Queue is Empty");
+            return -1;
+        }
+        else{
+            element = items[front];
+            if(front>=rear){
+                front=-1;
+                rear=-1;
+            }
+            else{
+                front++;
+
+            }
+            System.out.println("Deleted->"+element);
+            return element;
+        }   
+    }
+
+    void display(){
+        int i;
+        if(isEmpty()){
+            System.out.println("Queue is Empty");
+        }
+        else{
+            System.out.println("Front Index->"+front);
+           
+            for(i=front;i<=rear;i++){
+                System.out.print(items[i]+" ");
+            }
+             System.out.println("Rear Index->"+rear);
+        }
+    }
+    public static void main(String[] args) {
+    Queue q = new Queue();
+
+    // deQueue is not possible on empty queue
+    q.deQueue();
+
+    // enQueue 5 elements
+    q.enQueue(1);
+    q.enQueue(2);
+    q.enQueue(3);
+    q.enQueue(4);
+    q.enQueue(5);
+
+    // 6th element can't be added to because the queue is full
+    q.enQueue(6);
+
+    q.display();
+
+    // deQueue removes element entered first i.e. 1
+    q.deQueue();
+
+    // Now we have just 4 elements
+    q.display();
+
+  }
+
 }
