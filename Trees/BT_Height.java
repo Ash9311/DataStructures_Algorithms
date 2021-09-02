05b5bc832ee3d73206752846b0282d463b9f71aa
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2021-09-02 12:33:58 +0530

    Create BT_Height.java

---
+++ Trees/BT_Height.java
@@ -0,0 +1,44 @@
+//Max height of a tree is n-1 and min is floor(log2n)
+/* 
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+ */
+
+class Solution {
+    //Function to find the height of a binary tree.
+    int height(Node node) 
+    {
+        if(node==null){
+            return 0;
+        }
+        
+       return 1 + Math.max(height(node.left),height(node.right));
+    }
+}
+
+// class Solution {
+//     int height(Node node) {
+//         // code here 
+//         if(node==null){
+//             return 0;
+//         }
+//         int l_depth=height(node.left);
+//         int r_depth=height(node.right);
+        
+//         if(l_depth>r_depth){
+//             return l_depth+1;
+//         }
+//         else{
+//             return r_depth+1;
+//         }
+//     }
+// }
