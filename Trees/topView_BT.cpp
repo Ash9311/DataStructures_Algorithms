4b5e0a6483ee355dc79962a9eaee626ea4591d57
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2021-09-12 22:23:40 +0530

    Create topView_BT.cpp

---
+++ Trees/topView_BT.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    
+    map<int,pair<int,int>> m; // distance , pair of level and root data
+    
+    void helper( Node *root,int distance,int level){
+        if(root==NULL){
+            return;
+        }
+        if(m.find(distance)==m.end() || m[distance].first>level){ //map doesnt contain distance
+            m[distance].first = level;
+            m[distance].second = root->data;
+        
+        }
+        helper(root->left,distance-1,level+1);
+        helper(root->right,distance+1,level+1);
+        
+    }
+    
+    
+    vector<int> topView(Node *root)
+    {
+       vector<int> v;
+       m.clear();
+       helper(root,0,0);
+       for(auto it: m){
+           v.push_back(it.second.second);
+       }
+       return v;
+       
+    }
+    
+
+};
