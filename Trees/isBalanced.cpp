aa0f50594e8ad00b895e30f2e39e8220dc50db12
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2021-09-12 17:01:48 +0530

    Create isBalanced.cpp

---
+++ Trees/isBalanced.cpp
@@ -0,0 +1,36 @@
+/* A binary tree node class
+class Node
+{
+	int data;
+	Node left,right;
+	
+	Node(int d)
+	{
+		data = d;
+		left = right = null;		
+	}
+} */
+
+class Tree
+{
+    
+    //Function to check whether a binary tree is balanced or not.
+    boolean isBalanced(Node root)
+    {
+	// Your code here
+	if(root==null){
+	    return true;
+	}
+	int left = height(root.left);
+	int right = height(root.right);
+	return Math.abs(left-right)<2 && isBalanced(root.left) && isBalanced(root.right);
+    }
+    
+    int height(Node root){
+        if(root==null){
+            return 0;
+        }
+        return Math.max(height(root.left),height(root.right))+1;
+    }
+}
+
