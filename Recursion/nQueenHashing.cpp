ad4430f0809ebfc86f258f44466a36a54b5e4929
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-02-04 10:05:33 +0530

    Create nQueenHashing.cpp

---
+++ Recursion/nQueenHashing.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    
+    public:
+    void solve(int n,vector<string> &board,vector<vector<string>> &res,vector<int> &leftRow,vector<int> &lowerDiagonal,vector<int>&upperDiagonal,int col){
+        if(col==n){
+            res.push_back(board);
+            return;
+        }
+        for(int row=0;row<n;row++){
+            if(leftRow[row]==0 and lowerDiagonal[row+col]==0 and upperDiagonal[n-1 + col-row]==0){
+                board[row][col]='Q';
+                leftRow[row]=1;
+                lowerDiagonal[row+col]=1;
+                upperDiagonal[n-1 + col-row]=1;
+                solve(n,board,res,leftRow,lowerDiagonal,upperDiagonal,col+1);
+                board[row][col]='.';
+                leftRow[row]=0;
+                lowerDiagonal[row+col]=0;
+                upperDiagonal[n-1 + col-row]=0;
+                
+            }
+        }
+    }
+    
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> res;
+        vector<string>board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            board[i]=s;
+        }
+        vector<int> leftRow(n,0);
+        vector<int> lowerDiagonal(2*n - 1,0);
+        vector<int> upperDiagonal(2*n -1,0);
+        
+        solve(n,board,res,leftRow,lowerDiagonal,upperDiagonal,0);
+        return res;
+    }
+};
