fa9eacdcf123e34ce27f5ee5fa27c5fefbfceeb2
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-02-14 15:10:00 +0530

    Update FrogJump.cpp

--- Dynamic Programming/FrogJump.cpp
+++ Dynamic Programming/FrogJump.cpp
@@ -23,3 +23,28 @@ int frogJump(int n, vector<int> &heights)
     vector<int> dp(n+1,-1);
     return f(n-1,heights,dp);
 }
+
+
+
+
+//tabulation method
+
+#include<bits/stdc++.h>
+
+int frogJump(int n, vector<int> &heights)
+{
+   int prev=0;
+    int prev2=0;
+    
+    for(int i=1;i<n;i++){
+        int fs = prev + abs(heights[i]-heights[i-1]);
+        int ss = INT_MAX;
+        if(i>1){
+         ss = prev2 + abs(heights[i]-heights[i-2]);
+        }
+        int curr = min(fs,ss);
+        prev2=prev;
+        prev = curr;   
+    }
+    return prev;
+}
