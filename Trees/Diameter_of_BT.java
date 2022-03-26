a69edc8ec97037e82d22a0bbdb4d828fa1a45ff9
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-03-26 16:02:14 +0530

    Update Diameter_of_BT.java

--- Trees/Diameter_of_BT.java
+++ Trees/Diameter_of_BT.java
@@ -84,3 +84,23 @@ class Solution {
 // }
 
 
+
+// class Solution {
+// public:
+// int maxx=0;
+//     int maxDepth(TreeNode* root){
+//         if(root==NULL){
+//             return 0;
+//         }
+//         int left  = maxDepth(root->left);
+//         int right = maxDepth(root->right);
+//          maxx = max(left+right,maxx);
+//         return max(left,right)+1;
+//     }
+    
+//     int diameterOfBinaryTree(TreeNode* root) {
+//         maxDepth(root);
+//         return maxx;
+//     }
+// };
+
