6bad50953143895242c5af56dcab3691ca662bd4
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-04-14 10:15:11 +0530

    Update top_k_frequent_usingPQ.cpp

--- Hash/top_k_frequent_usingPQ.cpp
+++ Hash/top_k_frequent_usingPQ.cpp
@@ -1,4 +1,5 @@
 //https://leetcode.com/problems/top-k-frequent-elements/
+
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
@@ -8,13 +9,37 @@ public:
         }
         vector<int> res;
         priority_queue<pair<int,int>> pq;
-        for(auto it=map.begin();it!=map.end();it++){
-            pq.push(make_pair(it->second,it->first));
-            if(pq.size() > (int)map.size()-k){
-                res.push_back(pq.top().second);
-                pq.pop();
-            }
+        
+        for(auto it:map){
+            pq.push(make_pair(it.second,it.first));
         }
+        
+      for(int i=0;i<k;i++){
+          res.push_back(pq.top().second);
+          pq.pop();
+      }
+        
         return res;
     }
+};
+
+
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         unordered_map<int,int> map;
+//         for(int num: nums){
+//             map[num]++;
+//         }
+//         vector<int> res;
+//         priority_queue<pair<int,int>> pq;
+//         for(auto it=map.begin();it!=map.end();it++){
+//             pq.push(make_pair(it->second,it->first));
+//             if(pq.size() > (int)map.size()-k){
+//                 res.push_back(pq.top().second);
+//                 pq.pop();
+//             }
+//         }
+//         return res;
+//     }
 };
