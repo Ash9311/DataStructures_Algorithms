816eebd4537977092e0d5667ef759a7d282897a2
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-04-17 12:44:27 +0530

    Create pairs_absDifference.cpp

---
+++ Hash/pairs_absDifference.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/count-number-of-pairs-with-absolute-difference-k/
+
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) {
+        unordered_map<int,int>map;
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(map[nums[i]+k]){
+                count+=map[nums[i]+k];
+            }
+            if(map[nums[i]-k]){
+                count+=map[nums[i]-k];
+            }
+            map[nums[i]]++;
+        }
+        return count;
+    }
+};
