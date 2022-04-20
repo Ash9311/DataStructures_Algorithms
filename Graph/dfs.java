856e4b4b003f0447366adc4dd33ea72dfdb93df0
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-04-20 11:30:58 +0530

    Update dfs.java

--- Graph/dfs.java
+++ Graph/dfs.java
@@ -27,3 +27,35 @@ class Solution {
         return arr;
     }
 };
+
+
+//cpp
+class Solution {
+  public:
+    // Function to return a list containing the DFS traversal of the graph.
+    
+    void dfs(int node,vector<int> &vis,vector<int> adj[],vector<int> &storeDFS){
+        storeDFS.push_back(node);
+        vis[node]=1;
+        
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                vis[it]=1;
+                dfs(it,vis,adj,storeDFS);
+            }
+        }
+    }
+    
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        vector<int> storeDFS;
+        vector<int> vis(V+1,0);
+        
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                dfs(i,vis,adj,storeDFS);
+            }
+        }
+        return storeDFS;
+    }
+};
