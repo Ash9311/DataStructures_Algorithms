3d5c6cf4ab5a5a3ae103479ba4a865918496bdf0
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-04-26 10:35:27 +0530

    Update findDuplicate.cpp

--- Hash/findDuplicate.cpp
+++ Hash/findDuplicate.cpp
@@ -12,3 +12,29 @@ public:
         return 0;
     }
 };
+
+
+
+
+
+
+//another approach
+if (nums.size() > 1)
+	{
+		int slow = nums[0];
+		int fast = nums[nums[0]];
+		while (slow != fast)
+		{
+			slow = nums[slow];
+			fast = nums[nums[fast]];
+		}
+
+		fast = 0;
+		while (fast != slow)
+		{
+			fast = nums[fast];
+			slow = nums[slow];
+		}
+		return slow;
+	}
+	return -1;
