2284e7e35196d4d08e05aec0042e29c4b9bc97f0
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-04-30 12:18:46 +0530

    Update and rename DetectCycle_undirectedGraph_bfs.cpp to DetectCycle_undirectedGraph_bfsDfs.cpp

--- Graph/DetectCycle_undirectedGraph_bfsDfs.cpp
+++ Graph/DetectCycle_undirectedGraph_bfsDfs.cpp
@@ -41,3 +41,45 @@ class Solution {
     }
 };
 
+
+
+
+
+
+
+
+
+//DFS
+class Solution {
+  public:
+  
+  
+  bool isDetectCycle(int node,int parent,vector<int> adj[],vector<int> &vis){
+      vis[node]=1;
+      for(auto it:adj[node]){
+          if(vis[it]==0){
+              if(isDetectCycle(it,node,adj,vis)) return true;
+          }
+          else if(it!=parent){
+              return true;
+          }
+      }
+      return false;
+  }
+  
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int V, vector<int> adj[]) {
+        // Code here
+         vector<int> vis(V+1,0);
+        
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+            if(isDetectCycle(i,-1,adj,vis)){
+                return true;
+            }
+        }
+        }
+        return false;
+    }
+};
+
