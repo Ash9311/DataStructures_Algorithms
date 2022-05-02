eb244616096b1bcaa4083d2279ada4a73720b751
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-05-02 11:14:08 +0530

    Update biPartite_graph.cpp

--- Graph/biPartite_graph.cpp
+++ Graph/biPartite_graph.cpp
@@ -1,4 +1,6 @@
 //https://leetcode.com/problems/is-graph-bipartite/submissions/
+
+//BFS
 class Solution {
 public:
     
@@ -35,3 +37,51 @@ public:
         return true;
     }
 };
+
+
+
+
+
+
+//DFS
+class Solution {
+public:
+    
+    bool checkBipartiteDFS(int node,vector<int> &color,vector<vector<int>>& graph){
+      if(color[node]==-1){
+          color[node]=1;
+      }
+        for(auto it:graph[node]){
+            if(color[it]==color[node]){
+                return false;
+            }
+            else if(color[it]==-1){
+                color[it]=1-color[node];
+                if(!checkBipartiteDFS(it,color,graph)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> color(n,-1);
+        for(int i=0;i<n;i++){
+            if(color[i]==-1){
+                if(!checkBipartiteDFS(i,color,graph)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+
+
+
+
+
+
