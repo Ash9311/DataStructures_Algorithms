fc2a85fda346225230f67f9437b010881fc58a72
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-05-07 17:58:01 +0530

    Create shortestPathUndirectedGraph.cpp

---
+++ Graph/shortestPathUndirectedGraph.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+   void BFS(vector<int> adj[],int N,int src){
+       int dist[N];
+       for(int i=0;i<N;i++){
+           dist[i]=INT_MAX;
+       }
+       queue<int> q;
+       dist[src]=0;
+       q.push(src);
+       while(!q.empty()){
+           int node = q.front();
+           q.pop();
+           
+           for(auto it:adj[node]){
+               if(dist[node]+1 <dist[it] ){
+                   dist[it]=dist[node+1];
+                   q.push(it);
+               }
+           }
+       }
+   }
+};
