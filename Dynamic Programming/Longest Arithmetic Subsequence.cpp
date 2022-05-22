698617672e5d2815a1467dcd2cf6b99c23c53aba
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-05-22 17:33:41 +0530

    Update Longest Arithmetic Subsequence.cpp

--- Dynamic Programming/Longest Arithmetic Subsequence.cpp
+++ Dynamic Programming/Longest Arithmetic Subsequence.cpp
@@ -1,3 +1,13 @@
+
+The main idea is to maintain a map of differences seen at each index.
+
+We iteratively build the map for a new index i, by considering all elements to the left one-by-one.
+For each pair of indices (i,j) and difference d = A[i]-A[j] considered, we check if there was an existing chain at the index j with difference d already.
+
+If yes, we can then extend the existing chain length by 1.
+Else, if not, then we can start a new chain of length 2 with this new difference d and (A[j], A[i]) as its elements.
+At the end, we can then return the maximum chain length that we have seen so far.
+
 class Solution {
 public:
     int longestArithSeqLength(vector<int>& nums) {
