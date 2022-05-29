f12ea84a82390f84b995a1451c311b04e9049696
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-05-29 13:39:00 +0530

    Update BinaryToInteger.java

--- LinkedList/BinaryToInteger.java
+++ LinkedList/BinaryToInteger.java
@@ -10,6 +10,21 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int sum=0;
+        while(head!=NULL){
+            sum*=2;
+            sum+=head->val;
+            head = head->next;
+        }
+            return sum;
+    }
+};
+
+
 class Solution {
     
 //     public int BinaryToDecimal(int bn){
