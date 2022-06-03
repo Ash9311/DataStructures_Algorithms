32ef76341a04622c4ea9a6800ab603cb69a754c4
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-06-03 09:19:11 +0530

    Create next_permutation.cpp

---
+++ Arrays/next_permutation.cpp
@@ -0,0 +1,30 @@
+//declare 2 indexes. index1 is that point where the next element is greater from the end if its not there a reversed array is the ans. if not
+//find index2 which is the first greatest element from last w.r.t index1. swap it and reverse as mentioned in line 25.
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int index1,index2;
+        
+        for(index1=n-2;index1>=0;index1--){
+            if(nums[index1]<nums[index1+1]){
+                break;
+            }
+        }
+        
+        if(index1<0){
+            reverse(nums.begin(),nums.end());
+        }
+        else{
+            for(index2=n-1;index2>index1;index2--){
+                if(nums[index2]>nums[index1]){
+                    swap(nums[index1],nums[index2]);
+                    break;
+                }
+            }
+                reverse(nums.begin()+index1+1,nums.end());
+            
+        }
+    }
+};
