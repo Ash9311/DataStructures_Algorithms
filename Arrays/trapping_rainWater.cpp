80906210cea8407e9af7b399927a21d1b4cefe36
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-06-06 09:22:47 +0530

    Update trapping_rainWater.cpp

--- Arrays/trapping_rainWater.cpp
+++ Arrays/trapping_rainWater.cpp
@@ -1,4 +1,7 @@
 //https://leetcode.com/problems/trapping-rain-water/submissions/
+
+
+
 class Solution {
 public:
     int trap(vector<int>& height) {
@@ -26,5 +29,40 @@ public:
             
         }
         return result;
+    }
+};
+
+
+make an array left where each index is containing max value of current index height and the left most, similarly do it for right,
+ after that take the minimum of left and right and subtract it with current height, therefore you will be getting water that can be
+ stored in each block.
+
+
+// using arrays
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int left[n];
+        int right[n];
+        left[0]=height[0];
+        
+        for(int i=1;i<n;i++){
+            left[i]=max(left[i-1],height[i]);
+        }
+        
+        right[n-1]=height[n-1];
+        
+        for(int i=n-2;i>=0;i--){
+            right[i]=max(right[i+1],height[i]);
+        }
+        
+        int res=0;
+        
+        for(int i=0;i<n;i++){
+            res+=min(left[i],right[i])-height[i];
+        }
+        
+        return res;
     }
 };
