faecf2494534ea8640aebe74aee98c270e314f13
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-06-09 11:02:36 +0530

    Update 3sum.cpp

--- Arrays/3sum.cpp
+++ Arrays/3sum.cpp
@@ -1,3 +1,7 @@
+//https://leetcode.com/problems/3sum/submissions/
+
+iterate through each element and use 2 pointer approach to find the sum and also must handle duplicate numbers
+
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
@@ -34,6 +38,48 @@ public:
                 }
             }
         }
+        return result;
+    }
+};
+
+
+
+
+
+//approach 2
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        //[-4,-1,-1,0,1,2]
+        vector<vector<int>> result;
+        for(int i=0;i<n;i++){
+            if(i>0 and nums[i]==nums[i-1]){
+                continue;
+            }
+            int j = i+1;
+            int k = n-1;
+            
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                
+                if(sum==0){
+                    result.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                }
+                else if(sum>0){
+                    k--;
+                }
+                else{
+                    j++;
+                }   
+            }
+        }
+        //filters out duplicate vectors in 2D vector
+        result.erase(std::unique(result.begin(), result.end()), result.end());
         return result;
     }
 };
