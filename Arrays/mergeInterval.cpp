0bd72ce7a55d03abf9474b96d005289147f41117
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-06-11 23:53:55 +0530

    Update mergeInterval.cpp

--- Arrays/mergeInterval.cpp
+++ Arrays/mergeInterval.cpp
@@ -1,3 +1,7 @@
+//https://practice.geeksforgeeks.org/problems/8a644e94faaa94968d8665ba9e0a80d1ae3e0a2d/1/#
+
+//using vector tempInterval keep a track of second element(end) of the range. If an overlap is detected then update the second element of
+//tempInterval or else push it to resultant vector and proceed. 
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
@@ -9,18 +13,18 @@ public:
         
         sort(intervals.begin(),intervals.end());
         
-        vector<int> tempInterval = intervals[0];
+        vector<int> tempInterval = intervals[0]; // used as a previous vector
         
         for(auto it: intervals){
             if(it[0]<=tempInterval[1]){
-                tempInterval[1] = max(it[1], tempInterval[1]);
+                tempInterval[1] = max(it[1], tempInterval[1]); //overlap detected ,update the second index
             }
             else{
-                mergedInterval.push_back(tempInterval);
+                mergedInterval.push_back(tempInterval);// no overlap ,push the pair to the resultant vector;
                 tempInterval = it;
             }
         }
-        mergedInterval.push_back(tempInterval);
+        mergedInterval.push_back(tempInterval); // or else last pair that we found wont be considered
         return mergedInterval;
     }
 };
