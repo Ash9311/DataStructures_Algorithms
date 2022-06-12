390077e8a04b7387b0ea6691d5ea1c52eea2a0c5
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-06-12 22:39:24 +0530

    Update ParenthesesBalance.cpp

--- Stacks/ParenthesesBalance.cpp
+++ Stacks/ParenthesesBalance.cpp
@@ -1,61 +1,48 @@
-#include <bits/stdc++.h>
-using namespace std;
-class ParentheseBalance {
-    
+//https://leetcode.com/problems/valid-parentheses/submissions/
+
+//iterate through each and every char and check for opening and closing braces, if opening push it to the stack else check if it symmetric one
+//is at the stack.top()
+class Solution {
 public:
-    
     bool isValid(string s) {
         stack<int> st;
-        int n = s.size();
-        bool ans = true;
-        for(int i =0;i<n;i++){
-            if(s[i]=='[' or s[i]=='{' or s[i]=='('){
+        int n = s.length();
+        bool result = true;
+        for(int i=0;i<n;i++){
+            if(s[i]=='(' or s[i]=='{' or s[i]=='['){
                 st.push(s[i]);
             }
-            else if(s[i]==']'){
-                if(!st.empty() and st.top()=='['){
-                    st.pop();
-                }
-                else{
-                    ans = false;
-                    break;
-                }
-            }
-             else if(s[i]=='}'){
-                if(!st.empty() and st.top()=='{'){
-                    st.pop();
-                }
-                else{
-                    ans = false;
-                    break;
-                }
-            }
-             else if(s[i]==')'){
+            
+            else if(s[i]==')'){
                 if(!st.empty() and st.top()=='('){
                     st.pop();
                 }
                 else{
-                    ans = false;
-                    break;
+                    return false;
                 }
             }
-           
-        }
-         if(!st.empty()){
-                return false;
+            
+              else if(s[i]=='}'){
+                if(!st.empty() and st.top()=='{'){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
             }
-    return ans;
+            
+              else if(s[i]==']'){
+                if(!st.empty() and st.top()=='['){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(!st.empty()){
+            return false;
+        }
+        return true;
     }
-
-    
 };
-
-int main(){
-        ParentheseBalance obj;
-        bool ans = obj.isValid("{}");
-        std::cout << ans << std::endl;
-<<<<<<< HEAD
-    }
-=======
-    }
->>>>>>> origin/IP
