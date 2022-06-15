efdc650fc865c828672515d61b0ebe3561ce058b
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-06-15 09:14:30 +0530

    Create longest_Substr_wihtout_repeatingchar.cpp

---
+++ Strings/longest_Substr_wihtout_repeatingchar.cpp
@@ -0,0 +1,63 @@
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+maintain a set,make 2 references i.e i,j keep i fixed and move j forward. traverse through the array and see if there is a duplicate
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> set;
+        int i=0,j=0,ans=0;
+        int n = s.size();
+        while(i<n and j<n){
+            if(set.find(s[j])==set.end()){ //repeating char not found
+                set.insert(s[j]);
+                j++;
+                ans = max(ans,j-i);
+            }
+            else{
+                set.erase(s[i]);
+                i++;
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+//sliding window approace
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int i=0;
+        int j=0;
+        unordered_map<char,int> map;
+        int window_len =0;
+        int max = 0;
+        int start_window=i;
+        while(j<s.length()){
+            char ch = s[j];
+            
+            if(map.count(ch) and map[ch]>=i){
+               i = map[ch]+1;
+               window_len = j-i;
+            }
+            
+            map[ch] =j;
+            window_len++;         
+            j++;
+            
+            if(window_len > max){
+                max = window_len;
+                start_window = i;
+            }
+            
+        }
+        return max;
+    } 
+};
