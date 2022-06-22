54e79140dc90107aea007f65a484d24652f67cd7
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-06-22 17:31:50 +0530

    Update sum_of_pairs_greaterEquals_k.cpp

--- Sorting & Searching/sum_of_pairs_greaterEquals_k.cpp
+++ Sorting & Searching/sum_of_pairs_greaterEquals_k.cpp
@@ -1,11 +1,13 @@
 //https://practice.geeksforgeeks.org/problems/permutations-in-array1747/1/#
 
+//sort one array in ascending order and another on descending and then compare the sum of respective index.
+
 class Solution {
   public:
     bool isPossible(long long a[], long long b[], long long n, long long k) {
         // Your code goes here
         sort(a,a+n);
-        sort(b,b+n,greater<int>());
+        sort(b,b+n,greater<int>()); //sorts in descending
       
         for(int i=0;i<n;i++){
             if((a[i]+b[i])<k){
