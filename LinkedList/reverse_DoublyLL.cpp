ea3ef693a6e131c1278136ef8858a56b2ce6d5ac
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-07-01 10:36:55 +0530

    Create reverse_DoublyLL.cpp

---
+++ LinkedList/reverse_DoublyLL.cpp
@@ -0,0 +1,34 @@
+//https://practice.geeksforgeeks.org/problems/reverse-a-doubly-linked-list/1
+
+Node* reverseDLL(Node * head)
+{
+    //Your code here
+    if(head==NULL or head->next==NULL){
+        return head;
+    }
+    Node *temp = NULL;
+    Node *curr = head;
+    while(curr!=NULL){
+        temp = curr->prev;
+        curr->prev = curr->next;  //reversing   
+        curr->next = temp;      //reversing
+        curr = curr->prev; //since curr->prev is pointing to next node which we want to traverse
+        
+    }
+    return temp->prev;
+}
+
+struct Node
+{
+    int data;
+    Node * next;
+    Node * prev;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+        prev=NULL;
+    }
+        
+};
+*/
