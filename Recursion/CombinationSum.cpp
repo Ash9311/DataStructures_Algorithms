3e880e31e05bf3abc8967059c4b86f54e6424088
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-07-05 11:52:50 +0530

    Update CombinationSum.cpp

--- Recursion/CombinationSum.cpp
+++ Recursion/CombinationSum.cpp
@@ -27,3 +27,52 @@ public:
         return res;
     }
 };
+
+
+
+
+//https://practice.geeksforgeeks.org/problems/combination-sum-1587115620/1#
+//User function template for C++
+// class cmp{
+//     public:
+//  bool sizecom(const vector<int>& v1, const vector<int>& v2)
+// {
+    
+//     return v1.size() > v2.size();
+// }
+// };
+
+class Solution {
+  public:
+  
+ 
+  
+  void solve(vector<int> &A,int target,int i,vector<vector<int>> &result,vector<int>&ds){
+      if(i==A.size()){
+          if(target==0){
+              result.push_back(ds);
+          }
+          return;
+      }
+      
+      if(A[i]<=target){
+          ds.push_back(A[i]);
+          solve(A,target-A[i],i,result,ds);
+          ds.pop_back();
+      }
+      solve(A,target,i+1,result,ds); 
+  }
+  
+  
+    //Function to return a list of indexes denoting the required 
+    //combinations whose sum is equal to given number.
+    vector<vector<int> > combinationSum(vector<int> &A, int target) {
+        // Your code here
+        vector<vector<int>> result;
+        vector<int> ds;
+        sort(A.begin(),A.end());
+        A.erase(unique(A.begin(), A.end()), A.end()); //filter duplicate ones
+        solve(A,target,0,result,ds);
+        return result;
+    }
+};
