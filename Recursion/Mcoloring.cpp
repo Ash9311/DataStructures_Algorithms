b15eca022c3581e7639f2f151d2d6e2435eb2514
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-07-11 17:42:11 +0530

    Update Mcoloring.cpp

--- Recursion/Mcoloring.cpp
+++ Recursion/Mcoloring.cpp
@@ -1,35 +1,35 @@
 //https://practice.geeksforgeeks.org/problems/m-coloring-problem-1587115620/1#
+//start from node 0 and apply all colors and recursively check for all conditions satisfying different combinations
 
-bool isSafe(int node,bool graph[101][101],int col,int V,int color[]){
-    for(int k=0;k<V;k++){
-        if(k!=node and color[k]==col and graph[k][node]==1){
+bool isSafe(int node,int color[],bool graph[101][101],int n,int colr){
+    for(int k=0;k<n;k++){ //iterate through adjacent nodes
+        if(k!=node and  graph[k][node]==1 and color[k]==colr){
             return false;
         }
     }
     return true;
 }
 
-bool solve(int node,bool graph[101][101],int m,int V,int color[]){
-    if(node==V){
+bool solve(int node,int color[],bool graph[101][101],int m,int n){
+    if(node==n){  //reached the end without returning false
         return true;
     }
-    for(int i=1;i<=m;i++){
-        if(isSafe(node,graph,i,V,color)){
+    for(int i=1;i<=m;i++){ //try for all colors
+        if(isSafe(node,color,graph,n,i)){
             color[node]=i;
-            if(solve(node+1,graph,m,V,color)){
+            if(solve(node+1,color,graph,m,n)){
                 return true;
             }
-            color[node]=0;
-           
+            color[node]=0; //backtrack
         }
     }
-     return false;
+    return false;
 }
 
-bool graphColoring(bool graph[101][101], int m, int V)
-{
-    int color[V] = {0};
-    if(solve(0,graph,m,V,color)){
+bool graphColoring(bool graph[101][101], int m, int n) {
+    // your code here
+    int color[n]={0};
+    if(solve(0,color,graph,m,n)==true){
         return true;
     }
     return false;
