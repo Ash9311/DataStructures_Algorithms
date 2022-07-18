ecdb5f7caac052bb8ac85e65cb6e2cb21d381dd1
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-07-18 11:40:30 +0530

    Update TowerOfHanoi.cpp

--- Recursion/TowerOfHanoi.cpp
+++ Recursion/TowerOfHanoi.cpp
@@ -1,1 +1,14 @@
+//https://practice.geeksforgeeks.org/problems/tower-of-hanoi-1587115621/1?page=1&category[]=Recursion&sortBy=submissions
 
+//use recursive approach, in the second recursion change from,aux,to to aux,to,from, and in the end just return count
+class Solution{
+    public:
+    long long toh(int N, int from, int to, int aux) {
+        
+        if(N>0){
+            toh(N-1,from,aux,to);
+            cout<<"move disk "<<N<<" from rod "<<from<<" to rod "<<to<<"\n";
+            toh(N-1,aux,to,from);
+        }
+        return pow(2,N)-1;
+    }
