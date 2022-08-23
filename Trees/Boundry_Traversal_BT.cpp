38b8562f3b0ad8800f2c86dfe404ae4fcb56a27b
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-08-23 11:44:12 +0530

    Update Boundry_Traversal_BT.cpp

--- Trees/Boundry_Traversal_BT.cpp
+++ Trees/Boundry_Traversal_BT.cpp
@@ -79,3 +79,90 @@ public:
         return res;
     }
 };
+
+
+
+
+#2 little different approach
+
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+//first take the leftboundary but dont consider leaf nodes and then take the leaf nodes and then take rightbounday
+//while backtracking since we need right in reverse. and also handle condition where left node is empty and 
+//right node is non empty, in that case right node will act as left boundar
+class Solution {
+    
+    private:
+    void leftBoundary(Node *root,vector<int> &result){
+        
+         if(root==NULL){
+            return ;
+        }
+        if(root->left==NULL and root->right==NULL){
+            return;
+        }
+        result.push_back(root->data);
+        if(root->left){ //since there can be a case where left node isnt there and right nodes left will be a boundary
+            leftBoundary(root->left,result);
+        }
+        else{
+            leftBoundary(root->right,result);
+        }
+        
+        
+    }
+    
+    private:
+    void rightBoundary(Node *root,vector<int> &result){
+         if(root==NULL){
+            return ;
+        }
+        if(root->left==NULL and root->right==NULL){
+            return;
+        }
+       if(root->right){ //since there can be a case where right node isnt there and left nodes right will be a boundary
+        rightBoundary(root->right,result);
+       }
+       else{
+           rightBoundary(root->left,result);
+       }
+         result.push_back(root->data);
+    }
+    
+    private:
+    void leafNodes(Node *root,vector<int> &result){
+         if(root==NULL){
+            return ;
+        }
+        if(root->left==NULL and root->right==NULL){
+            result.push_back(root->data);
+            return;
+        }
+        leafNodes(root->left,result);
+        leafNodes(root->right,result);
+    }
+    
+    
+    
+public:
+    vector <int> boundary(Node *root)
+    {
+        vector<int> result;
+        if(root==NULL){
+            return result;
+        }
+         if(root->left==NULL and root->right==NULL){ //if there is only 1 node
+            result.push_back(root->data);
+            return result;
+        }
+        result.push_back(root->data); //insert root and then go for left,right
+        leftBoundary(root->left,result);
+        leafNodes(root,result);
+        rightBoundary(root->right,result);
+        
+        return result;
+    }
+};
