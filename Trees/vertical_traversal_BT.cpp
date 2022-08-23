22d4224149b59d25a90d0fdc81539e720a95de14
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-08-23 17:33:45 +0530

    Update vertical_traversal_BT.cpp

--- Trees/vertical_traversal_BT.cpp
+++ Trees/vertical_traversal_BT.cpp
@@ -1,3 +1,68 @@
+
+//https://practice.geeksforgeeks.org/problems/print-a-binary-tree-in-vertical-order/1
+class Solution
+{
+    public:
+    
+     //mark an imaginary horizontal line like .... -2 -1 0 1 2....with main root pointing 0. make a map and queue
+     //for tracking and traversal.
+    
+    //Function to find the vertical order traversal of Binary Tree.
+    vector<int> verticalOrder(Node *root)
+    {
+           map<int,map<int,vector<int>>> nodes;
+ //Horizontal dist^     ^level      ^list of nodes
+       queue<pair<Node*,pair<int,int>>> q; //queue for level order traversal and tracking purpose
+       //                      ^horizontal dist,level of nodes    
+       vector<int> ans;
+       
+       if(root==NULL){
+           return ans;
+       }
+       q.push(make_pair(root,make_pair(0,0)));//hd,lvl is 0 for root node
+       
+       while(!q.empty()){
+           pair<Node*,pair<int,int>> temp = q.front(); //take all node from queue and process it
+           q.pop();
+           Node* frontNode = temp.first;
+           int hd = temp.second.first;
+           int lvl = temp.second.second;
+           
+           nodes[hd][lvl].push_back(frontNode->data);
+           
+           if(frontNode->left!=NULL){
+               q.push(make_pair((frontNode->left),make_pair(hd-1,lvl+1)));
+           }
+            if(frontNode->right!=NULL){
+               q.push(make_pair((frontNode->right),make_pair(hd+1,lvl+1)));
+           }
+           
+       }
+       
+       for(auto i:nodes){
+           for(auto j:i.second){
+               for(auto k:j.second){
+                   ans.push_back(k);
+               }
+           }
+       }
+       return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
 
 /**
