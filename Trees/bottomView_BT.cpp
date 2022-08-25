36c9605fe24642cbc3f4162831108419ba7ae83c
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-08-25 10:37:57 +0530

    Update bottomView_BT.cpp

--- Trees/bottomView_BT.cpp
+++ Trees/bottomView_BT.cpp
@@ -1,3 +1,49 @@
+//https://practice.geeksforgeeks.org/problems/bottom-view-of-binary-tree/1
+
+//maintain a map of horizontal distance and bottom node data and a queue of pair of node,hd. first push root to queue.
+//pop the font node and hd. and then keep on overwriting bottomNode data which belongs to the same hd. and then proceed with
+//inserting left and right nodes to the queue
+class Solution {
+  public:
+    vector <int> bottomView(Node *root) {
+        // Your Code Here
+        vector<int> ans;
+        if(root==NULL){
+            return ans;
+        }
+        map<int,int> bottomNode; //pair of horizontal distance and bottom node data
+        queue<pair<Node*,int>>q; //int is hd
+        q.push(make_pair(root,0));//pair of root and horizontal distance
+        
+        while(!q.empty()){
+            pair<Node*,int> temp = q.front();
+            q.pop();
+            Node* frontNode = temp.first;
+            int hd = temp.second;
+            
+            bottomNode[hd]=frontNode->data; //this will overwrite the node data for that hd, thus while traversing
+            //we will get deepest nodes for that horizontal distance
+            
+            if(frontNode->left){ //continue traversing
+                q.push(make_pair(frontNode->left,hd-1));
+            }
+             if(frontNode->right){
+                q.push(make_pair(frontNode->right,hd+1));
+            }
+        }
+        
+        for(auto it: bottomNode){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
+
+
+
+
+//#2 using recursion
+
 class Solution {
   public:
   
