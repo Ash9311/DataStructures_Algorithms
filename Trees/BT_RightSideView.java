6f8a72bbdbe8b3f3972591a1871101d7b1d4aff0
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-08-25 11:00:56 +0530

    Update BT_RightSideView.java

--- Trees/BT_RightSideView.java
+++ Trees/BT_RightSideView.java
@@ -1,3 +1,38 @@
+//cpp https://practice.geeksforgeeks.org/problems/right-view-of-binary-tree/1
+//traverse recursively and check lvl==ans.size() condition to determine first node of new level, traverse right first.
+class Solution
+{
+    public:
+    
+    void solve(Node *root,vector<int> &ans,int lvl){
+    if(root==NULL){
+        return;
+    }
+  
+    //by this condition we get to know that we entered a new level
+    if(lvl==ans.size()){
+        ans.push_back(root->data);
+    }
+    solve(root->right,ans,lvl+1); //traverse right first and increment lvl
+    solve(root->left,ans,lvl+1);
+    
+}
+    //Function to return list containing elements of right view of binary tree.
+    vector<int> rightView(Node *root)
+    {
+        vector<int> ans;
+   solve(root,ans,0);
+   return ans;
+    }
+};   
+
+
+
+
+
+
+
+//java
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
