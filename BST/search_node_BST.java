d1c90fad361f7431e382833a9ca579d95b6c6e4d
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-09-04 23:19:48 +0530

    Update search_node_BST.java

--- BST/search_node_BST.java
+++ BST/search_node_BST.java
@@ -1,6 +1,22 @@
 //https://practice.geeksforgeeks.org/problems/search-a-node-in-bst/1/#
+//cpp
+bool searchInBST(BinaryTreeNode<int> *root, int x) {
+    if(root==NULL){
+        return false;
+    }
+    if(root->data==x){
+        return true;
+    }
+    if(root->data<x){
+        searchInBST(root->right,x);
+    }
+    else if(root->data>x){
+        searchInBST(root->left,x);
+    }
+}
 
 
+//java
 class BST {
     // Function to search a node in BST.
     boolean search(Node root, int x) {
