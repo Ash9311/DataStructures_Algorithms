d7d158c4cfdcc84c01b670dacc3fe83813a02ac7
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-09-08 11:18:38 +0530

    Create LCA BST.cpp

---
+++ BST/LCA BST.cpp
@@ -0,0 +1,17 @@
+//https://www.codingninjas.com/codestudio/problems/lca-in-a-bst_981280?leftPanelTab=1
+
+//in case of BST its different. first go to subtree where LCA CAN LIE
+TreeNode<int>* LCAinaBST(TreeNode<int>* root, TreeNode<int>* P, TreeNode<int>* Q)
+{
+	if(root==NULL){
+        return NULL;
+    }
+    if(root->data<P->data and root->data<Q->data){//indicates that LCA is in right subtree
+        return LCAinaBST(root->right,P,Q);
+    }
+     if(root->data>P->data and root->data>Q->data){//indicates that LCA is in left subtree
+        return LCAinaBST(root->left,P,Q);
+    }
+   return root;//if root->Data > P and root->Data <Q then the root is LCA (AND vice versa)
+    
+}
