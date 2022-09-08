2d56819bbb2c787d094a205b0507bb4bfe03d9ba
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-09-08 11:31:11 +0530

    Update LCA BST.cpp

--- BST/LCA BST.cpp
+++ BST/LCA BST.cpp
@@ -15,3 +15,26 @@ TreeNode<int>* LCAinaBST(TreeNode<int>* root, TreeNode<int>* P, TreeNode<int>* Q
    return root;//if root->Data > P and root->Data <Q then the root is LCA (AND vice versa)
     
 }
+
+
+//iterative
+
+TreeNode<int>* LCAinaBST(TreeNode<int>* root, TreeNode<int>* P, TreeNode<int>* Q)
+{
+	if(root==NULL){
+        return NULL;
+    }
+    while(root!=NULL){
+         if(root->data<P->data and root->data<Q->data){//indicates that LCA is in right subtree
+           root = root->right;
+    }
+     else if(root->data>P->data and root->data>Q->data){//indicates that LCA is in left subtree
+        root = root->left;
+    }
+        else{
+            return root;
+        }
+    }
+    
+    
+}
