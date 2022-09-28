c54c227e1f205a71a142c58b42471febd10ce68f
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-09-28 15:46:29 +0530

    Update kth largest sum subarray.cpp

--- Heaps/kth largest sum subarray.cpp
+++ Heaps/kth largest sum subarray.cpp
@@ -1,5 +1,5 @@
 //https://www.codingninjas.com/codestudio/problems/k-th-largest-sum-contiguous-subarray_920398?leftPanelTab=0&utm_source=youtube&utm_medium=affiliate&utm_campaign=Lovebabbar
-
+//use a min heap, insert all sub array sum into it. make sure min heap size is less than K. the top element of min heap is our required ans.
 #include<queue>
 int getKthLargest(vector<int> &arr, int k)
 {
@@ -9,12 +9,12 @@ int getKthLargest(vector<int> &arr, int k)
     for(int i=0;i<n;i++){
         int sum = 0;
         for(int j=i;j<n;j++){
-            sum+=arr[j];
-            if(mini.size()<k){
+            sum+=arr[j]; //each and every combination is added to min heap
+            if(mini.size()<k){ //since we need kth largest. it needs to b at top
                 mini.push(sum);
             }
             else{
-                if(sum>mini.top()){
+                if(sum>mini.top()){ //update if larger num is present
                     mini.pop();
                     mini.push(sum);
                 }
