46b9cd4b6a30d3f94ed0346026b8e0ca7da192eb
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-11-20 23:49:16 +0530

    Update Unique paths.cpp

--- Dynamic Programming/Unique paths.cpp
+++ Dynamic Programming/Unique paths.cpp
@@ -22,3 +22,23 @@ int uniquePaths(int m, int n) {
     vector<vector<int>> dp(m,vector<int>(n,-1));
 	return f(m-1,n-1,dp);
 }
+
+
+//tabulation
+int uniquePaths(int m, int n) {
+    int dp[m][n];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(i==0 and j==0){
+                dp[i][j] = 1;
+            }
+            else{
+                int up=0,left=0;
+                if(i>0) up = dp[i-1][j];
+                if(j>0) left = dp[i][j-1];
+                dp[i][j] = up + left;
+            }
+        }
+    }
+    return dp[m-1][n-1];
+}
