42138d08495f051f1c0f60baf4d4d580bff0bd8b
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-11-24 12:53:28 +0530

    Update Triangle.cpp

--- Dynamic Programming/Triangle.cpp
+++ Dynamic Programming/Triangle.cpp
@@ -21,4 +21,44 @@ public:
         vector<vector<int>> dp(n,vector<int>(n,-1));
         return solve(0,0,n,triangle,dp);
     }
+    
+    ----------------
+    //tabulation method
+        //T.C -> O(n*n)  S.C-> O(n*n)
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<vector<int>> dp(n,vector<int>(n,0));
+       for(int col=0;col<n;col++){
+           dp[n-1][col] = triangle[n-1][col]; //fill the last row first
+       }
+        for(int row=n-2;row>=0;row--){ //do it for remaining
+            for(int col=row;col>=0;col--){
+                int down = triangle[row][col] + dp[row+1][col];
+                int diagonal = triangle[row][col] + dp[row+1][col+1];
+                dp[row][col] = min(down,diagonal);
+            }
+        }
+        return dp[0][0];
+    }
+    
+    
+     //space optimization 
+    //T.C -> O(n*n)  S.C-> O(n)
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<int> front(n,0), curr(n,0);
+       for(int col=0;col<n;col++){
+           front[col] = triangle[n-1][col]; //fill the last row first
+       }
+        for(int row=n-2;row>=0;row--){ //do it for remaining
+            for(int col=row;col>=0;col--){
+                int down = triangle[row][col] + front[col];
+                int diagonal = triangle[row][col] + front[col+1];
+                curr[col] = min(down,diagonal);
+            }
+            front = curr;
+        }
+        return front[0];
+    }
+    
 };
