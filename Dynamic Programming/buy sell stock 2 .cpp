cca21df61f14cd71ee8e8006451b9688c03bdf4c
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2022-12-26 11:02:38 +0530

    Update buy sell stock 2 .cpp

--- Dynamic Programming/buy sell stock 2 .cpp
+++ Dynamic Programming/buy sell stock 2 .cpp
@@ -33,3 +33,41 @@ public:
         return solve(0,prices,buy,dp);
     }
 };
+
+
+//with transaction fee
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/description/
+class Solution {
+public:
+
+    int solve(int i,vector<int>& prices,int fee,int n,int buy,vector<vector<int>> &dp){
+
+        if(i==n){
+            return 0;
+        }
+
+        if(dp[i][buy]!=-1){
+            return dp[i][buy];
+        }
+
+        int profit = 0;
+        if(buy){
+            int purchased = -prices[i] + solve(i+1,prices,fee,n,0,dp);
+            int notPurchased = solve(i+1,prices,fee,n,1,dp);
+            profit = max(purchased,notPurchased);
+        }
+        else{
+            int sell = prices[i] -fee + solve(i+1,prices,fee,n,1,dp);
+            int dontSell = solve(i+1,prices,fee,n,0,dp);
+            profit = max(sell,dontSell);
+        }
+        return dp[i][buy]=profit;
+    }
+
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+        int buy = 1;
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+        return solve(0,prices,fee,n,buy,dp);
+    }
+};
