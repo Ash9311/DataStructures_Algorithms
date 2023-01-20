5fd4199abb8d0353ef7f6c7139be9e8a55d5d98c
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2023-01-20 12:45:15 +0530

    Update biPartite_graph.cpp

--- Graph/biPartite_graph.cpp
+++ Graph/biPartite_graph.cpp
@@ -1,3 +1,55 @@
+
+//https://practice.geeksforgeeks.org/problems/bipartite-graph/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=bipartite-graph
+//do bfs on first node. give any color initially lets say '1' and push it to the queue. note it and pop
+//check for its neighbours. if neighbour is not colored then color it with opposite of current
+// -1 is uncolored, 1 & 0 are the colors that v have
+//basically if the cycle in a graph is odd then its not bi-partite
+//tc as same as BFS
+class Solution {
+    
+private:
+
+bool checkBipartiteBFS(int i,vector<int> &color,vector<int>adj[]){
+    queue<int> q;
+    q.push(i);
+    color[i] = 1;
+   
+    while(!q.empty()){ //process queue elements
+        int currNode = q.front();
+        q.pop();
+        
+     for(auto it: adj[currNode]){ //process neighbours
+        if(color[it]==color[currNode]){ //if neighbour's color is already same then its not bipartite
+            return false;
+        }
+        if(color[it]==-1){ //if not color then color it with opposite
+            color[it] = 1-color[currNode];
+            q.push(it);
+        }
+    }
+    }
+    
+    return true;
+}
+    
+public:
+	bool isBipartite(int V, vector<int>adj[]){
+	    vector<int> color(V,-1);
+	  
+	    for(int i=0;i<V;i++){
+	        if(color[i]==-1){
+	            if(!checkBipartiteBFS(i,color,adj)){
+	                return false;
+	            }
+	        }
+	    }
+	    return true;
+	}
+
+};
+
+
+
 //https://leetcode.com/problems/is-graph-bipartite/submissions/
 
 //BFS
