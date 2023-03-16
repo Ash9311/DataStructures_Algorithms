041a5ee76aff71667f6152101e99eba6c411bb36
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2023-03-16 23:55:35 +0530

    Update experiments.cpp

--- playAround/experiments.cpp
+++ playAround/experiments.cpp
@@ -127,3 +127,56 @@ Len=String.valueOf(a).length();
         return (String.join("",word1).equals(String.join("",word2)));
     }
 }
+
+
+const data = {
+  "users": [
+    {
+      "id": 1,
+      "name": "John",
+      "age": 25,
+      "hobbies": ["reading", "gaming"],
+      "address": {
+        "street": "123 Main St",
+        "city": "Anytown",
+        "state": "CA",
+        "zip": "12345"
+      }
+    },
+    {
+      "id": 2,
+      "name": "Jane",
+      "age": 30,
+      "hobbies": ["hiking", "photography"],
+      "address": {
+        "street": "456 Oak St",
+        "city": "Someville",
+        "state": "NY",
+        "zip": "54321"
+      }
+    },
+    {
+      "id": 3,
+      "name": "Bob",
+      "age": 35,
+      "hobbies": ["cooking"],
+      "address": {
+        "street": "789 Elm St",
+        "city": "Othercity",
+        "state": "FL",
+        "zip": "67890"
+      }
+    }
+  ]
+};
+
+// Filter users ov…
+In this example, we have a JSON object with an array of users. We use the filter() method to filter the users over the age of 30, and then the map() method to return an array of objects containing only the user's name and hobbies. The resulting filteredUsers array will contain two objects representing users Jane and Bob:
+[  {    "name": "Jane",    "hobbies": ["hiking", "photography"]
+  },
+  {
+    "name": "Bob",
+    "hobbies": ["cooking"]
+  }
+]
+data.users.filter(user => user.hobbies.includes("gaming"));  //user whose hobbies include gaming
