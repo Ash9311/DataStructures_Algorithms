a35b9d3e4b4da7d23302d2e148d79b83aca33b18
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2023-04-08 15:39:46 +0530

    Update pascalsTriangle.cpp

--- 2D-Arrays/pascalsTriangle.cpp
+++ 2D-Arrays/pascalsTriangle.cpp
@@ -1,6 +1,6 @@
 https://leetcode.com/problems/pascals-triangle/description/
 
-//for each row first declare a vector of size o tht row with value filled with 1. then iterate that row and fill values depending on the previous 
+//for each row first declare a vector of size of tht row with value filled with 1. then iterate that row and fill values depending on the previous 
 //row if there and push that row vector to the resultant 2d vector.
 
 class Solution {
@@ -8,7 +8,7 @@ public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> res;
         for(int i=0;i<numRows;i++){
-            vector<int> row(i+1,1); //first declare a vector of size o tht row with value filled with 1
+            vector<int> row(i+1,1); //first declare a vector of size of tht row with value filled with 1
             for(int j=1;j<i;j++){ 
                 row[j]=res[i-1][j]+res[i-1][j-1];
             }
