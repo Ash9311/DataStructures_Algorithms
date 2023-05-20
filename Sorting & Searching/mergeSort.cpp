3da50a8bb54e3fc1960b46f55c859c10dfb78acc
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2023-05-20 16:47:12 +0530

    Update mergeSort.cpp

--- Sorting & Searching/mergeSort.cpp
+++ Sorting & Searching/mergeSort.cpp
@@ -1,3 +1,69 @@
+//https://practice.geeksforgeeks.org/problems/merge-sort/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+class Solution
+{
+    public:
+    void merge(int arr[], int left, int mid, int right)
+    {
+         int leftSize = mid - left +1;
+         int rightSize = right - mid;
+         
+         //create temp arrays
+         vector<int> leftArr(leftSize);
+         vector<int> rightArr(rightSize);
+         
+         //copy data to temporary arrays
+         for(int i=0;i<leftSize;i++){
+             leftArr[i]=arr[left+i];
+         }
+         for(int j=0;j<rightSize;j++){
+             rightArr[j]=arr[mid+j+1];
+         }
+         
+         //merge temp arrays back to arr
+         int i=0;
+         int j =0;
+         int k = left;
+         
+         while(i<leftSize && j<rightSize){ //2 pointer approach sort
+             if(leftArr[i] <= rightArr[j]){
+                 arr[k] = leftArr[i];
+                 i++;
+             }
+             else{
+                 arr[k] = rightArr[j];
+                 j++;
+             }
+             k++;
+         }
+         
+         //include remaining indexes , willl execute either
+         while(i<leftSize){
+             arr[k] = leftArr[i];
+             i++;
+             k++;
+         }
+          while(j<rightSize){
+             arr[k] = rightArr[j];
+             j++;
+             k++;
+         }
+    }
+    public:
+    void mergeSort(int arr[], int left, int right)
+    {
+        //code here
+        if(left<right){
+            int middle = left + (right-left)/2;
+            //sort first and second halves
+            mergeSort(arr,left,middle);
+            mergeSort(arr,middle+1,right);
+            merge(arr,left,middle,right);
+        }
+    }
+};
+--------
+    
+    
 class Solution {
 public:
     
