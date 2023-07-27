bfd5a810968482c5985875b45ddb16a19adc5267
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2023-07-27 13:05:56 +0530

    Update query-2d-matrix.cpp

--- 2D-Arrays/query-2d-matrix.cpp
+++ 2D-Arrays/query-2d-matrix.cpp
@@ -1,7 +1,11 @@
 //https://leetcode.com/problems/range-sum-query-2d-immutable/description/
 //first create a prefix matrix.
 
-[[[[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]],[2,1,4,3],[1,1,2,2],[1,2,2,4]]
+3 0 1 4 2 
+5 6 3 2 1
+1 2 0 1 5
+4 1 0 1 7
+1 0 3 0 5
     
 3 3 4 8 10 
 8 14 18 24 27 
