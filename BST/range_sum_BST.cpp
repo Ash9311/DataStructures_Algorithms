1857324ffcbcd79923c901aba059ea256d75df4c
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2023-09-24 23:53:26 +0530

    Update range_sum_BST.cpp

--- BST/range_sum_BST.cpp
+++ BST/range_sum_BST.cpp
@@ -30,3 +30,19 @@ public:
         return sum;
     }
 };
+
+
+
+------------------------------------//M2
+
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(root==NULL) return 0;
+        int sum = 0;
+        if(root->val>=low && root->val<=high) sum+=root->val;
+        sum+=rangeSumBST(root->left,low,high);
+        sum+=rangeSumBST(root->right,low,high);
+        return sum;
+    }
+};
