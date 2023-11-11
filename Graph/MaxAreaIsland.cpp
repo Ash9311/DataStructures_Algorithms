550282fcd63d9e489692f097b121c446eb7dd747
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2023-11-11 23:06:16 +0530

    Update MaxAreaIsland.cpp

--- Graph/MaxAreaIsland.cpp
+++ Graph/MaxAreaIsland.cpp
@@ -1,44 +1,42 @@
 //https://leetcode.com/problems/max-area-of-island/
 class Solution {
-    
-    public: int maxx=0;
 public:
-    
-    void bfs(vector<vector<int>> &grid,int i,int j,vector<vector<int>> &vis,int n,int m){
-        queue<pair<int,int>> q;
+int maxx = 0;
+    void bfs(int i,int j,int n,int m,vector<vector<int>>& grid,vector<vector<int>>& vis){
+        queue<pair<int,int>>q;
         q.push({i,j});
-        int count=1;
+        int count = 1;
         vis[i][j]=1;
-    
-        int dx[] = {0,0,1,-1};
-        int dy[] = {-1,1,0,0};
-        while(!q.empty()){
-            int i=q.front().first;
-            int j=q.front().second;
+        int dx[] = {0,1,-1,0};
+        int dy[] = {1,0,0,-1};
+
+        while(!q.empty()){ //process each cell
+            int row = q.front().first;
+            int col = q.front().second;
             q.pop();
-            for(int idx=0;idx<4;idx++){
-                int newi=i+dx[idx];
-                int newj=j+dy[idx];
-                
-                if(newi>=0 and newj>=0 and newi<n and newj<m and !vis[newi][newj] and grid[newi][newj]){
-                    q.push({newi,newj});
-                    vis[newi][newj]=1;
+            //explore the neighbours
+            for(int i=0;i<4;i++){
+                int nrow = row + dx[i];
+                int ncol = col + dy[i];
+
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && !vis[nrow][ncol]){  //validate
+                    q.push({nrow,ncol});
+                    vis[nrow][ncol]=1;
                     count++;
-                }
+                } 
             }
-            
         }
-       maxx=max(maxx,count);
+        maxx = max(maxx,count);
     }
-    
+
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>> vis(n,vector<int>(m,0));
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){ //iterate through grid and if its a land then apply bfs
             for(int j=0;j<m;j++){
-                if(vis[i][j]==0 and grid[i][j]==1){
-                    bfs(grid,i,j,vis,n,m);
+                if(grid[i][j]==1){ 
+                    bfs(i,j,n,m,grid,vis);
                 }
             }
         }
